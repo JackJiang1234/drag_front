574ca1379479ad88f5217cf240662b8f1b3dcfca
jianyong.jiang@starlight-sms.com <jianyong.jiang@starlight-sms.com>
2019-04-24 12:22:30 +0800

    refomate code

--- src/java/inter/If.java
+++ src/java/inter/If.java
@@ -3,8 +3,9 @@ package inter;
 import symbols.Type;
 
 /**
+ * if语句中间代码生成
  * if (expr) stmt
- * */
+ */
 public class If extends Stmt {
 
     Expr expr;
@@ -13,7 +14,9 @@ public class If extends Stmt {
     public If(Expr x, Stmt s) {
         expr = x;
         stmt = s;
-        if (expr.type != Type.Bool) expr.error("boolean required in if");
+        if (expr.type != Type.Bool) {
+            expr.error("boolean required in if");
+        }
     }
 
     public void gen(int b, int a) {
--- src/java/inter/Stmt.java
+++ src/java/inter/Stmt.java
@@ -2,7 +2,7 @@ package inter;
 
 /**
  * 表示语句结点生成
- * */
+ */
 public class Stmt extends Node {
 
     public Stmt() {
@@ -11,8 +11,11 @@ public class Stmt extends Node {
     public static Stmt Null = new Stmt();
 
     /**
-     * 生成中间代码
-     * */
+     * 生成中间代码,
+     *
+     * @param b 标记当前语句开始
+     * @param a 标记当前语句的代码之后的第一条指令
+     */
     public void gen(int b, int a) {
     } // called with labels begin and after
 
